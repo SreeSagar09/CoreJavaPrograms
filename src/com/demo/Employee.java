e9dc2168c578a96eb02dd4fa661f6045799d318d
SAGAR <SAGAR@DESKTOP-F0I5IAU>
2022-04-21 15:21:17 +0530

    Employee Class is Added.

---
+++ src/com/demo/Employee.java
@@ -0,0 +1,69 @@
+package com.demo;
+
+public class Employee {
+	private String empId;
+	private String ename;
+	private Integer age;
+	private Double sal;
+	private String dept;
+	
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(String empId, String ename, Integer age, Double sal, String dept) {
+		super();
+		this.empId = empId;
+		this.ename = ename;
+		this.age = age;
+		this.sal = sal;
+		this.dept = dept;
+	}
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Double getSal() {
+		return sal;
+	}
+
+	public void setSal(Double sal) {
+		this.sal = sal;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
